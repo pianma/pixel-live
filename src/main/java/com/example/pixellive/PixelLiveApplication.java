f0ca448a5bf831d39ac0e786f87f1e291ae493bd
KimGeonWoo <ikkw2040@naver.com>
2026-04-26 20:21:36 +0900

    최초 커밋

---
+++ src/main/java/com/example/pixellive/PixelLiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.pixellive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pixellive/PixelLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pixellive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
