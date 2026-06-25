bb4905ebda714b92c86efe33cbd0a1b1e34a50ae
kimgeonwoo <ikkw2040@naver.com>
2026-06-25 16:31:52 +0900

    ErrorCode 롬복사용

--- src/main/java/com/pixellive/common/exception/ErrorCode.java
+++ src/main/java/com/pixellive/common/exception/ErrorCode.java
@@ -1,6 +1,11 @@
 package com.pixellive.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
     INVALID_COORDINATE(HttpStatus.BAD_REQUEST, "C001", "캔버스 범위를 벗어난 좌표입니다."),
     INVALID_COLOR_CODE(HttpStatus.BAD_REQUEST, "C002", "유효하지 않은 색상 코드입니다. (0~15)"),
@@ -10,22 +15,4 @@ public enum ErrorCode {
     private final HttpStatus status;
     private final String code;
     private final String message;
-
-    ErrorCode(HttpStatus status, String code, String message) {
-        this.status = status;
-        this.code = code;
-        this.message = message;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
